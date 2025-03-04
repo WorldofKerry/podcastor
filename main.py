17e9db144b91a514fe391ed666162dc0477e559f
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-04 14:41:13 -0800

    use podcast, fix flake8

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def run(api_key, content):
     messages=[
         {
         "role": "user",
-        "content": content + "\n Give Me An In Depth Summary"
+        "content": content + "\n Convert This Into A Podcast Conversation Without Losing Nuances"
         }
     ]
     )
