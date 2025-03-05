1a6f7747db3a84ec59bee11fda19f795c21d3288
Kerry Wang <kerrywang369@gmail.com>
2025-03-04 17:38:41 -0800

    minor

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,8 +1,5 @@
 from gtts import gTTS
-import time
 
 def to_audio(content, path):
-    print("to_audio", content[:100], len(content), path)
     tts = gTTS(content)
     tts.save(path)
-    print("done")
