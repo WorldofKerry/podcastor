6759d732879cf3691c501b244ba5484e181a46ce
Kerry Wang <kerrywang369@gmail.com>
2025-03-04 17:32:46 -0800

    try gtts

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,10 +1,8 @@
-import pyttsx3
+from gtts import gTTS
 import time
 
 def to_audio(content, path):
     print("to_audio", content[:100], len(content), path)
-    engine = pyttsx3.init()
-    engine.save_to_file(content[:100] , path)
-    engine.runAndWait()
-    time.sleep(20)
+    tts = gTTS(content)
+    tts.save(path)
     print("done")
