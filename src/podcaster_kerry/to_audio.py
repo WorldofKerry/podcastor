221d379042b36adb35e38c12fb611907d1780b6a
Kerry Wang <kerrywang369@gmail.com>
2025-03-04 17:41:19 -0800

    minor

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,5 +1,7 @@
 from gtts import gTTS
 
 def to_audio(content, path):
+    # FUTURE: https://stackoverflow.com/questions/37600197/custom-python-gtts-voice
+    # Use localizations / accents to distinguish between different speakers
     tts = gTTS(content)
     tts.save(path)
