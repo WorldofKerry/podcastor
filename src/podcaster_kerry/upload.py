d07ac64567934ac16007f6724ef8d0c74f9305b6
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-04 16:11:09 -0800

    use current time

--- src/podcaster_kerry/upload.py
+++ src/podcaster_kerry/upload.py
@@ -1,9 +1,9 @@
 import requests
+import datetime
 
 def upload(file_path):
     url = "https://worldofkerry-server.vercel.app/upload"
-    file_id = "podcast_audio"
-
+    file_id = "podcast audio " + str(datetime.datetime.now())
     with open(file_path, "rb") as file:
         files = {"file": file}
         data = {"file_id": file_id}
