e1cc26bed8dde1e6c787180cdaf75ec0b9a99da6
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-04 16:27:41 -0800

    rmv all voice stuff for now

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -2,11 +2,5 @@ import pyttsx3
 
 def to_audio(content, path):
     engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    for voice in voices:
-        engine.setProperty('voice', voice.id)
-        engine.say('The quick brown fox jumped over the lazy dog.')
-
-    engine.runAndWait()
-
     engine.save_to_file(content , path)
+    engine.runAndWait()
