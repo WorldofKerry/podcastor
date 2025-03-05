b574e178a874604c50d38f93ade66ba852507616
Kerry Wang <kerrywang369@gmail.com>
2025-03-04 18:00:58 -0800

    minor

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,6 +1,20 @@
 from gtts import gTTS
+import re
+
+def parse_text(content: str) -> list[tuple[str, str]]:
+    """
+    ret[i] represents the i-th dialogue
+    ret[i][0] is the speaker name
+    ret[i][1] is the speaker content
+    """
+    content = content.strip() + "\n" # Ensure single newline at end
+    pattern = r"(H\d):\s*(.*?)\n(?=(H\d:)|$)"
+    matches = re.findall(pattern, content, re.DOTALL)
+    print(matches)
+    return [(speaker, dialogue.strip()) for speaker, dialogue, _ in matches]
 
 def to_audio(content, path):
+    result = parse_text(content)
     # FUTURE: https://stackoverflow.com/questions/37600197/custom-python-gtts-voice
     # Use localizations / accents to distinguish between different speakers
     tts = gTTS(content)
