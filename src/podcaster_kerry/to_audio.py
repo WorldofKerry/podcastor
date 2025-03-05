57e1d068f77bc09a9579fc62a463b20ec022f90c
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-04 16:43:12 -0800

    add debugging

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,6 +1,8 @@
 import pyttsx3
 
 def to_audio(content, path):
+    print("to_audio", content[:10], path)
     engine = pyttsx3.init()
     engine.save_to_file(content , path)
     engine.runAndWait()
+    print("done")
