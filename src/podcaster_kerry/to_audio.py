4ed0a9a62b13825227f3853b697a48e8b4e11cda
Kerry Wang <kerrywang369@gmail.com>
2025-03-04 17:25:23 -0800

    limit content

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,8 +1,10 @@
 import pyttsx3
+import time
 
 def to_audio(content, path):
-    print("to_audio", content[:20], len(content), path)
+    print("to_audio", content[:100], len(content), path)
     engine = pyttsx3.init()
-    engine.save_to_file(content , path)
+    engine.save_to_file(content[:100] , path)
     engine.runAndWait()
+    time.sleep(20)
     print("done")
