28e0ad6aa9b7de5796452db36c4e9a10b8215e79
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-06 13:43:12 -0800

    fix test

--- test/test_piper.py
+++ test/test_piper.py
@@ -8,7 +8,7 @@ def test_create_piper_json():
     H1: Absolutely. The heart of the TPU is this massive 65,536 8-bit MAC matrix multiply unit, which gives it a peak throughput of 92 TeraOps per second. That’s insane!  
     H2: Right? And they’ve got this 28 MiB software-managed on-chip memory. What’s interesting is how they’ve optimized for deterministic execution, which is crucial for meeting the 99th-percentile response-time requirements of their applications.  
     """
-    result = create_piper_json(content)
+    result = create_piper_json(SAMPLE)
     json_str = "\n".join(json.dumps(r) for r in result)
     print(json_str)
 
