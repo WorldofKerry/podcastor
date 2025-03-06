d791c0ff6251075dd1b91f7b1cd5525dfd9d0127
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-06 13:36:57 -0800

    create piper json

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,6 +1,24 @@
 from gtts import gTTS
 import re
 
+def create_piper_json(content: str) -> list[dict]:
+    """
+    Return: follows format specified here: https://github.com/rhasspy/piper?tab=readme-ov-file#json-input
+    { "text": "First speaker.", "speaker_id": 0, "output_file": "/tmp/speaker_0.wav" }
+    """
+    ret = []
+    parsed = parse_text(content)
+    speaker_mapping: dict[str, int]
+    id_counter = 0
+
+    for speaker, content in parsed:
+        if speaker not in speaker_mapping:
+            speaker_mapping[speaker] = id_counter
+            id_counter += 1
+        speaker_id = speaker_mapping[speaker]
+        ret.append({"text": content, "speaker_id": speaker_id})
+    return ret
+
 def parse_text(content: str) -> list[tuple[str, str]]:
     """
     ret[i] represents the i-th dialogue
@@ -14,7 +32,9 @@ def parse_text(content: str) -> list[tuple[str, str]]:
     return [(speaker, dialogue.strip()) for speaker, dialogue in matches]
 
 def to_audio(content, path):
-    result = parse_text(content)
+    result = create_piper_json(content)
+    json_str = "\n".join(json.dumps(r) for r in result)
+    print(json_str)
     # FUTURE: https://stackoverflow.com/questions/37600197/custom-python-gtts-voice
     # Use localizations / accents to distinguish between different speakers
     tts = gTTS(content)
