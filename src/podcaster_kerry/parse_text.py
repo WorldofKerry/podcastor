cd98a12fabda4e2f3762ecc93739ec3451349d71
Kerry Wang <kerrywang369@gmail.com>
2025-03-06 18:17:30 -0800

    seems good

--- src/podcaster_kerry/parse_text.py
+++ src/podcaster_kerry/parse_text.py
@@ -1,6 +1,9 @@
 
-def pdf_to_text(path):
+import os
+
+
+def pdf_to_text(path: os.PathLike) -> str:
     import tika
     from tika import parser
-    parsed = parser.from_file(path)
+    parsed = parser.from_file(str(path))
     return parsed["content"]
