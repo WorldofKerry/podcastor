118ee3c38250c61e056496215e345318069afa3f
Kerry Wang <kerrywang369@gmail.com>
2025-03-06 17:49:53 -0800

    piper able to produce multiple output segments

--- src/podcaster_kerry/to_audio.py
+++ src/podcaster_kerry/to_audio.py
@@ -1,7 +1,33 @@
+from dataclasses import dataclass
+import subprocess
 from gtts import gTTS
 import re
 
-def create_piper_json(content: str) -> list[dict]:
+# American english voices with > 1 speaker. https://github.com/rhasspy/piper/blob/9b1c6397698b1da11ad6cca2b318026b628328ec/src/python_run/piper/voices.json#L4
+KEY_TO_NUM_SPEAKERS = {"en_US-libritts-high": 904, "en_US-arctic-medium": 18, "en_US-l2arctic-medium": 24}
+
+@dataclass
+class Entry:
+    speaker_id: int
+    text: str
+
+# Default values from https://github.com/rhasspy/piper/blob/master/src/cpp/piper.hpp
+@dataclass
+class PiperParameters:
+    length_scale: float = 1.0 # Phenome length (lower if faster)
+    noise_scale: float = 0.667 # Generator noise
+    noise_w: float = 0.8 # Phonme width noise
+    sentence_silence: float = 0.0 # Seconds of silence after each sentence
+
+    def as_args(self) -> list[str]:
+        return [
+            "--length-scale", str(self.length_scale),
+            "--noise-scale", str(self.noise_scale),
+            "--noise-w", str(self.noise_w),
+            "--sentence-silence", str(self.sentence_silence),
+        ]
+
+def _make_entries(content: str) -> list[Entry]:
     """
     Return: follows format specified here: https://github.com/rhasspy/piper?tab=readme-ov-file#json-input
     { "text": "First speaker.", "speaker_id": 0, "output_file": "/tmp/speaker_0.wav" }
@@ -16,8 +42,26 @@ def create_piper_json(content: str) -> list[dict]:
             speaker_mapping[speaker] = id_counter
             id_counter += 1
         speaker_id = speaker_mapping[speaker]
-        ret.append({"text": content, "speaker_id": speaker_id})
+        ret.append(Entry(speaker_id, content))
     return ret
+
+def get_audio(content: str):
+    results = _make_entries(content)
+    model = "en_US-arctic-medium"
+    num_speakers = KEY_TO_NUM_SPEAKERS[model]
+
+    for i, entry in enumerate(results):
+        if entry.speaker_id > num_speakers:
+            print(f"Speaker ID {entry.speaker_id} is out of range for model {model}, modifiying to {entry.speaker_id % num_speakers}")
+            entry.speaker_id = entry.speaker_id % num_speakers
+        parameters = PiperParameters()
+        command = ["piper",
+                "--model", model,
+                "--speaker", str(entry.speaker_id),
+                "--output-file", f"outputs/output_{i}.wav",
+                *parameters.as_args(),
+                ]
+        _ = subprocess.check_output(command, input=entry.text.encode())
 
 def parse_text(content: str) -> list[tuple[str, str]]:
     """
--- test/test_piper.py
+++ test/test_piper.py
@@ -1,10 +1,7 @@
-from podcaster_kerry.to_audio import create_piper_json
+from podcaster_kerry.to_audio import get_audio
+import subprocess
 from . import SAMPLE
-import json
 
 def test_create_piper_json():
-    result = create_piper_json(SAMPLE)
-    json_str = "\n".join(json.dumps(r) for r in result)
-    print(json_str)
-
+    get_audio(SAMPLE)
     assert False
