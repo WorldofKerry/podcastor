70771118efb2f7b18ea6ba9c9b4fea78cbb28598
Kerry Wang <kerrywang369@gmail.com>
2025-03-06 18:03:58 -0800

    more robust test

--- test/test_piper.py
+++ test/test_piper.py
@@ -1,7 +1,20 @@
+import os
+import shutil
 from pathlib import Path
 from podcaster_kerry.to_audio import get_audio
 from . import SAMPLE
 
 def test_create_piper_json():
-    pass
-    get_audio(SAMPLE, Path("./outputs/"))
+    if os.getenv("GITHUB_ACTIONS"):
+        temp_dir = Path("./outputs/")
+        shutil.rmtree(temp_dir, ignore_errors=True)
+        assert not temp_dir.exists()
+
+        output = Path("./combined.wav")
+        output.unlink(missing_ok=True)
+        assert not output.exists()
+
+        get_audio(SAMPLE, temp_dir, output)
+
+        assert temp_dir.exists()
+        assert output.exists()
