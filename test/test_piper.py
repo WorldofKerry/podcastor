2aca95419beb38223a5d9428446062032ef60326
Kerry Wang <kerrywang369@gmail.com>
2025-03-06 18:04:51 -0800

    only run on github actions

--- test/test_piper.py
+++ test/test_piper.py
@@ -3,18 +3,19 @@ import shutil
 from pathlib import Path
 from podcaster_kerry.to_audio import get_audio
 from . import SAMPLE
+import pytest
 
+@pytest.mark.skipif(not os.getenv("GITHUB_ACTIONS"), reason="Only run on GitHub Actions")
 def test_create_piper_json():
-    if os.getenv("GITHUB_ACTIONS"):
-        temp_dir = Path("./outputs/")
-        shutil.rmtree(temp_dir, ignore_errors=True)
-        assert not temp_dir.exists()
+    temp_dir = Path("./outputs/")
+    shutil.rmtree(temp_dir, ignore_errors=True)
+    assert not temp_dir.exists()
 
-        output = Path("./combined.wav")
-        output.unlink(missing_ok=True)
-        assert not output.exists()
+    output = Path("./combined.wav")
+    output.unlink(missing_ok=True)
+    assert not output.exists()
 
-        get_audio(SAMPLE, temp_dir, output)
+    get_audio(SAMPLE, temp_dir, output)
 
-        assert temp_dir.exists()
-        assert output.exists()
+    assert temp_dir.exists()
+    assert output.exists()
