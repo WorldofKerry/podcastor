638d02db9ac1e8ac22e492ac0dfa7f5c15299adb
Kerry Wang <kerrywang369@gmail.com>
2025-03-08 12:35:55 -0800

    xtts sounds better

--- src/podcaster_kerry/audio.py
+++ src/podcaster_kerry/audio.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+import os
 from pathlib import Path
 import subprocess
 import wave
@@ -91,13 +92,16 @@ def _dialogue_to_mp3_piper(entries: list[Entry], segments_dir: Path):
         _ = subprocess.check_output(command, input=entry.text.encode())
 
 def _dialogue_to_mp3_coqui(entries: list[Entry], segments_dir: Path):
+    os.environ["COQUI_TOS_AGREED=1"] = "1"
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    tts = TTS("tts_models/en/vctk/vits").to(device)
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
     for i, entry in enumerate(entries):
+        entry.speaker_id += 23 # Dislike first speakers
         entry.speaker_id = entry.speaker_id % len(tts.speakers)
         tts.tts_to_file(
             text=entry.text,
             speaker=tts.speakers[entry.speaker_id],
+            language="en",
             file_path=segments_dir / f"{i}.wav",
         )
 
