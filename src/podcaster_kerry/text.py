e2a5f2e1b61dc01d079901ecad5fda13f5b539a1
Kerry Wang <kerrywang369@gmail.com>
2025-03-07 19:24:01 -0800

    silence tika logger

--- src/podcaster_kerry/text.py
+++ src/podcaster_kerry/text.py
@@ -1,9 +1,11 @@
-
 import os
+import logging
+from tika import parser
 
+# Silence tika logs https://github.com/chrismattmann/tika-python/issues/341
+tika_logger = logging.getLogger("tika.tika")
+tika_logger.setLevel(logging.WARNING)
 
 def extract_text(path: os.PathLike) -> str:
-    import tika
-    from tika import parser
     parsed = parser.from_file(str(path))
     return parsed["content"]
