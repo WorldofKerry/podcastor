f6680d6eb18563566903ea532adb33bc84572143
Kerry Wang <kerrywang369@gmail.com>
2025-03-08 12:44:26 -0800

    fix env var

--- src/podcaster_kerry/audio.py
+++ src/podcaster_kerry/audio.py
@@ -9,6 +9,8 @@ from pydub import AudioSegment
 import torch
 from TTS.api import TTS
 
+os.environ["COQUI_TOS_AGREED"] = "1"
+
 SEGMENTS_DIR = "segments"
 WAVE_FILE = "combined.wav"
 
@@ -92,7 +94,6 @@ def _dialogue_to_mp3_piper(entries: list[Entry], segments_dir: Path):
         _ = subprocess.check_output(command, input=entry.text.encode())
 
 def _dialogue_to_mp3_coqui(entries: list[Entry], segments_dir: Path):
-    os.environ["COQUI_TOS_AGREED=1"] = "1"
     device = "cuda" if torch.cuda.is_available() else "cpu"
     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
     for i, entry in enumerate(entries):
