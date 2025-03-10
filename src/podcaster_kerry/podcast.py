e15eb10c70533cba97bde92020b84e0daecd924d
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-10 13:28:56 -0700

    update prompt

--- src/podcaster_kerry/podcast.py
+++ src/podcaster_kerry/podcast.py
@@ -2,7 +2,9 @@ from openai import OpenAI
 
 PROMPT_TEXT = """
 Convert this into a two-party podcast conversation.
-Be specific on equations used, new findings, and methodologies, etc. Assume the target audience is highly technical.
+Be specific on equations used, new findings, and methodologies, etc.
+Assume the target audience is highly technical.
+
 Avoid latex in the result, as they are difficult to understand in audio format.
 Aim to generate 10 minutes worth of content.
 
@@ -11,7 +13,7 @@ H1: <things to say>
 H2: <things to say>
 H1: <things to say>
 ...
-where a newline delimits the contents.
+where a newline is the delimiter.
 """
 
 
